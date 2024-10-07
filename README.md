1f865890727cd712060217e9a12b2205f736d561
th3poli <jan.zagorski04@gmail.com>
2024-10-07 13:09:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# polinv
+ Simple Python with collected in one place many modules with extra configurations and useful tools.
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='polinv',
+    version='1.0.0',
+    description='Simple Python with collected in one place many modules with extra configurations and useful tools.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author='th3poli',
+    author_email='',
+    keywords=['polinv', 'helpful', 'tool'],
+    url='https://github.com/th3poli/polinv',
+    install_requires=['requests', 'beautifulsoup4']
+)
+
+# python setup.py sdist bdist_wheel
+# pip install ./dist/polinv-1.0.0.tar.gz --no-cache-dir
+# python -m twine upload .\dist\*
